881da76b64cd741e362719ab45d41b2de2273c79
kks <drather@naver.com>
2024-11-03 17:44:17 +0900

    proxy pattern - ex7: decorator pattern, apply class based proxy 2

--- src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteClientTest.java
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteClientTest.java
@@ -3,6 +3,7 @@ package hello.proxy.pureproxy.concreteproxy;
 import com.sun.tools.jconsole.JConsoleContext;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,5 +18,15 @@ class ConcreteClientTest {
         concreteClient.execute();
     }
 
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+
+        ConcreteClient concreteClient = new ConcreteClient(timeProxy);
+
+        concreteClient.execute();
+    }
 
 }
