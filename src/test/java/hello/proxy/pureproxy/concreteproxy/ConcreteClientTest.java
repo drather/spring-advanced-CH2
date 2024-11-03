f0cb524212f27f2db867175f7728bc49919f610b
kks <drather@naver.com>
2024-11-03 17:35:41 +0900

    proxy pattern - ex7: decorator pattern, apply class based proxy 1 (fix test code file location )

--- src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteClientTest.java
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteClientTest.java
@@ -1,6 +1,8 @@
-package hello.proxy.pureproxy.concreteproxy.code;
+package hello.proxy.pureproxy.concreteproxy;
 
 import com.sun.tools.jconsole.JConsoleContext;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
